d3786d5865c00489f49c96754c2e07847e50d1ae
Equipped <93303641+InFutiveDev@users.noreply.github.com>
2025-02-05 19:27:10 +0530

    update banner

--- layout/about/csr/CsrBanner.js
+++ layout/about/csr/CsrBanner.js
@@ -63,7 +63,7 @@ const CsrBanner = () => {
           src={BannerImage}
         />
       </div>
-      <div className="relative w-full  items-center flex  sm:hidden">
+      {/* <div className="relative w-full  items-center flex  sm:hidden">
         <Image
           quality={100}
           width={1000}
@@ -73,7 +73,7 @@ const CsrBanner = () => {
           className=" object-fill md:mb-0 mb-3  h-auto w-full object-center "
           src={MobileImage}
         />
-      </div>
+      </div> */}
     </div>
   );
 };
