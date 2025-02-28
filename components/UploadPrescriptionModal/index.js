8e95dbd4f5a19cdb92f0deeccd70d66457b08588
Equipped <93303641+InFutiveDev@users.noreply.github.com>
2025-02-28 15:06:55 +0530

    update prescription

--- components/UploadPrescriptionModal/index.js
+++ components/UploadPrescriptionModal/index.js
@@ -234,7 +234,7 @@ const UploadPrescriptionModal = ({ open, setOpen }) => {
                 className="flex flex-col items-center justify-center w-full h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
               >
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <Image src={ImageOne} className=" Prescription icon" alt="Prescription icon" />
+                  <img src={ImageOne} className=" Prescription icon" alt="Prescription icon"  />
                   <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
                     <span className="font-semibold">Click to upload</span>
                   </p>
@@ -266,7 +266,9 @@ const UploadPrescriptionModal = ({ open, setOpen }) => {
                         }
                         alt="add_prescription"
                         // height={"100%"}
-                        className="h-full w-full"
+                        width={150} // Add width
+                        height={150} // Add height
+                        className="h-full w-full object-cover"
                       />
                       {item?.type.includes("pdf") && (
                         <p className="line-clamp-2 text-lg">{item?.name}</p>
