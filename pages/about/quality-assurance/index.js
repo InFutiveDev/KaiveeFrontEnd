bd6eead86bff769b0bb6c7b518b27cfa316897bf
InFutiveDev <93303641+InFutiveDev@users.noreply.github.com>
2025-03-05 18:26:55 +0530

    Update index.js

--- pages/about/quality-assurance/index.js
+++ pages/about/quality-assurance/index.js
@@ -4,10 +4,10 @@ import Head from "next/head";
 
 const QualityAssurance = () => {
   const metaData = {
-    title: "Our Quality Assurance | City X-Ray",
-    description: "Renowned for Accurate & Reliable Diagnostic Testing. Our Quality Assurance Ensures Excellence Throughout - City X-Ray",
-    url: "https://www.cityxrayclinic.com/about/quality-assurance", // Replace with the canonical URL of the page
-    image: "https://www.cityxrayclinic.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fqulity%20assurance%201000%20x%20600.1db4f769.webp&w=1080&q=100", // Replace with the actual image URL
+    title: "Our Quality Assurance",
+    description: " Kaivee Heallthcare Renowned for Accurate & Reliable Diagnostic Testing. Our Quality Assurance Ensures Excellence Throughout ",
+    url: "https://www.kaiveehealthcare.com/", // Replace with the canonical URL of the page
+    image: "", // Replace with the actual image URL
   };
 
   return (
@@ -22,11 +22,11 @@ const QualityAssurance = () => {
         <meta property="og:type" content="website" />
         <meta property="og:url" content={metaData.url} />
         <meta property="og:image" content={metaData.image} />
-        <meta property="og:site_name" content="City X-Ray" />
+        <meta property="og:site_name" content="Kaivee Heallthcare" />
 
         {/* Twitter meta tags */}
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@CityXrayClinic" />
+        <meta name="twitter:site" content="@Kaivee Heallthcare" />
         <meta name="twitter:title" content={metaData.title} />
         <meta name="twitter:description" content={metaData.description} />
         <meta name="twitter:image" content={metaData.image} />
