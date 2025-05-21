a2cc55e42e9d4d07ec19d16964adc057dbdcb7ec
Equipped <93303641+InFutiveDev@users.noreply.github.com>
2025-05-21 17:57:05 +0530

    update location map and number

--- layout/home/index.js
+++ layout/home/index.js
@@ -617,10 +617,10 @@ const locationData = [
     mainTilte: "Kaivee HealthCare Noida",
     address: "Noida,K-4, Sector-116, U.P-201305",
     serviceAvailable: "MRI Scan, CT Scan, Digital X-Ray, Neurology Test, etc",
-    contactNumber: "084483 38674",
+    contactNumber: "9289088633",
     timing: "24 hours open",
     email: "care@zenhealthsolutions.com",
     iframe:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.1278876753586!2d77.3960429!3d28.5659222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cef823d515f91%3A0xd4d4ace124409340!2sZen%20Health%20Services!5e0!3m2!1sen!2sin!4v1737958352844!5m2!1sen!2sin",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.1183944972404!2d77.3973573!3d28.5662073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cef0218a5d79d%3A0xb9e21e2afdcc53f6!2sKaivee-%20Zen%20Health%20%7C%20Pathology%20Lab%20%7C%20Diagnostic%20Centre%20%7C%20Healthcare%20Lab%20%7C%20Noida!5e0!3m2!1sen!2sin!4v1747830202578!5m2!1sen!2sin",
   }
 ];
